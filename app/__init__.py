5688db863c536b14be194bd3e8dfcf1f4b723f30
Felix N <felix.n.nunez@gmail.com>
2020-01-30 19:35:40 -0500

    added sample quiz

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-db = SQLAlchemy()
-migrate = Migrate()
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-    db.init_app(app)
-    migrate.init_app(app, db)
-    return app
-
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes, models

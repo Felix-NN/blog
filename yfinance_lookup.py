1f1ea4742cec56c5bfcea9e6cfd8ac933800b646
Felix N <felix.n.nunez@gmail.com>
2021-02-11 21:19:13 -0500

    Added websites to Dataframe

--- yfinance_lookup.py
+++ yfinance_lookup.py
@@ -1,6 +1,7 @@
 #%%
 from numpy.core.fromnumeric import std
 import yfinance as yf
+import random
 import pandas as pd
 import numpy as np
 from googlesearch import search
@@ -23,40 +24,55 @@ def download_history(symbol):
 def spikes(data):
     stocks = []
     variance = []
-    spike_dates = []
+    spike_dates = {}
     for dates, price in data.items():
+        date = dates.strftime('%Y-%m-%d')
         stocks.append(price)
         if len(stocks) > 7:
             variance.append(std(stocks[-8:-1]))
         else:
             variance.append(std(stocks))
         
-        if len(stocks) == 1:
+        if len(stocks) <= 2:
+            spike_dates[date] = 0
             continue
-        elif len(stocks) == 2:
-            pre_var = price - stocks[0]
         else:
             pre_var = price - stocks[-2]
         if abs(pre_var) > (variance[-2] * 3):
-            date = dates.strftime('%y-%m-%d')
-            spike_dates.append(date)
-    i = 0
-    for item in spike_dates:
-        print(i, item)
-        i += 1
-    i = 0
-    #while i < len(stocks):
-    #    print(data.index[i], stocks[i], variance[i])
-    #    i += 1
-# %%
-#def add_sites(data):
+            spike_dates[date] = 1
+        else:
+            spike_dates[date] = 0
+    spike_dates = pd.DataFrame.from_dict(spike_dates, orient='index', columns=['Spikes'])
+    data = data.to_frame().join(spike_dates)
+    return data
     
 # %%
-company = 'google'
+def add_sites(data, company):
+    spike_dates = data.loc[data['Spikes'] == 1]
+    web_dates = {}
+    for date, value in spike_dates.iterrows():
+        date = date.strftime('%Y-%m-%d')
+        company_search = company + ' stock ' + date
+        rand = random.randrange(150, 350)
+        rand = float(rand)
+        rand = rand/100
+        websites = []
+        for res in search(company_search, tld='com', lang='en', num=10, pause=rand, stop=10, tpe='nws'):
+            websites.append(res)
+        web_dates[date] = websites
+    web_dates = pd.Series(web_dates, name='Websites')
+    data = data.join(web_dates)
+    return data
+        
+        
+    
+# %%
+company = 'nike'
 symbol = ticker(company)
 data = download_history(symbol)
-spikes(data)
+data = spikes(data)
 #data.plot()
 # %%
-
+data = add_sites(data, company)
+print(data)
 # %%
